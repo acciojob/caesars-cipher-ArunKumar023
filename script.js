0ca4b7bf8b570bd3b65d22f57978d199c2f8f11f
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-19 15:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,53 @@
-function rot13(str) {
-  // Define a helper function to shift a single character by 13 places
-  function shiftChar(char) {
-    // Get the ASCII code of the character
-    let code = char.charCodeAt(0);
-    // Shift the code by 13 places, wrapping around if necessary
-    if (code >= 65 && code <= 90) {
-      code = ((code - 65 + 13) % 26) + 65;
-    }
-    // Convert the shifted code back to a character
-    return String.fromCharCode(code);
-  }
+var lookup = {
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
+};
 
-  // Use map and join to apply the shiftChar function to each character in the string
-  return str.split('').map(shiftChar).join('');
+function rot13(encodedStr) {
+  var words = encodedStr.split(" "); // String to Array
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    let decoded_word = "";
+    for (let j = 0; j < word.length; j++) {
+      var char = word.charAt(j);
+      var decoded_char = lookup[char];
+      decoded_word += decoded_char;
+    }
+    decodedArr.push(decoded_word);
+  }
+  return decodedArr.join(" ");
 }
+
+// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
+
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
+
+module.exports = rot13;
