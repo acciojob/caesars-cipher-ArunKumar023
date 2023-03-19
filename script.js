649470f9e333e38ed0528906ae0eaadf91d8fcb4
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-03-19 15:03:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-// Your Script here.
 function rot13(str) {
-  var result = "";
-  for (var i = 0; i < str.length; i++) {
-    var charCode = str.charCodeAt(i);
-    if (charCode >= 65 && charCode <= 90) {
-      // uppercase letters only
-      charCode = ((charCode - 65 + 13) % 26) + 65;
+  // Define a helper function to shift a single character by 13 places
+  function shiftChar(char) {
+    // Get the ASCII code of the character
+    let code = char.charCodeAt(0);
+    // Shift the code by 13 places, wrapping around if necessary
+    if (code >= 65 && code <= 90) {
+      code = ((code - 65 + 13) % 26) + 65;
     }
-    result += String.fromCharCode(charCode);
+    // Convert the shifted code back to a character
+    return String.fromCharCode(code);
   }
-  return result;
+
+  // Use map and join to apply the shiftChar function to each character in the string
+  return str.split('').map(shiftChar).join('');
 }
